caa2b30a323f400f594cb0a4a9f4f41b2ee98822
DAN786000 <danishjmohammad@gmail.com>
2026-06-19 08:20:21 -0700

    Added problem 4

---
+++ Chapter 2 Practice set/04_problem.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    // Explain step by step evaluation of 3*x/y - z+k, where x=2, y=3, z=3, k=1 
+    int x=2;
+    int y=3;
+    int z=3;
+    int k=1;
+    //3*x/y - z+k
+    //6/y - z+k
+    //2 - z+k
+    //-1+k
+    //0
+
+
+    return 0;
+}
