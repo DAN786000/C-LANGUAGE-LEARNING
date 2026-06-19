35eaa48b342386feb4dc495f8c54ca32065636e6
DAN786000 <danishjmohammad@gmail.com>
2026-06-18 22:26:21 -0700

    Added Chapter 1 programs

---
+++ Chapter 1/variables.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    //i am writing this code just for fun
+    int a; //variable declare 
+    a=8; //varible initialize
+    printf("The output of the program is %d",a);
+    return 0;
+}   
