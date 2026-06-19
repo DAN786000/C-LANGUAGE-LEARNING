c196b00e2d10972b7ea6954803c937d753c8d2e2
DAN786000 <danishjmohammad@gmail.com>
2026-06-19 07:32:34 -0700

    Added topic 1

---
+++ Chapter 2/01_variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int i=10;
+    int j=i;
+    int a=1,b=2,c=3,d=4;
+    printf("The value of i is %d and the value of j is %d\n",i,j);
+    printf("The value of a is %d and the value of b is %d\n",a,b);
+    printf("The value of c is %d and the value of d is %d\n",c,d);
+    return 0;
+}
