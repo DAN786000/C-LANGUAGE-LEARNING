a3b6f7854ae58ba03bc319d6b4b10d3ec4ec4102
DAN786000 <danishjmohammad@gmail.com>
2026-06-19 08:51:19 -0700

    Added problem 4

---
+++ Chapter 4 Practice set/04_problem4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    /*  What can be done using one type of loop can also be done using the other two 
+types of loops – true or false? */
+     //True.
+     //Yes the answer is true because because a work can be done using one type of loop can also be done using the other two types of loops.
+    return 0;
+}
