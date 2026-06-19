e5d754b1344ad4876ffe9869a265d2ebbb79f441
DAN786000 <danishjmohammad@gmail.com>
2026-06-19 08:26:32 -0700

    Added topic 4

---
+++ Chapter 3/04_logical.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a= 1, b= 1;
+
+    printf("The value of a and b is %d\n", a && b);
+    printf("The value of a and b is %d\n", a || b);
+    printf("The value of not(a) is %d\n", !a);
+    
+    if (a && b);
+    printf("Both the values are true\n");
+
+    if(a){
+        if(b)
+        printf("Both the values are true\n");
+
+        return 0;
+    }
+
+}
+
+    
