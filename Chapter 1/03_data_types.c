04fbc6324ced30a7557919ccc31c3627f25ff957
DAN786000 <danishjmohammad@gmail.com>
2026-06-18 22:40:55 -0700

    Adding all files

---
+++ Chapter 1/03_data_types.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // int a;
+    // a=1
+
+    int a = 1;
+
+    //float b;
+    //b = 2;
+
+    float b = 2.5;
+
+   // char c;
+    //c = 'd';
+
+    char c = 'd';
+
+    printf("the value of a is  %d\n",a);
+    printf("the value of b is %f\n",b);
+    printf("the value of c is %c\n",c);
+    return 0;
+}
---
+++ Chapter 1/05_comments.c
@@ -0,0 +1,14 @@
+/*  
+Program:05_comments.c
+Author:Danish
+Date:13 april 2005
+*/ 
+#include <stdio.h>
+
+int main()
+{
+    //i am writing this line just for fun it doesn't do anything
+    printf("Hello World");
+    printf("Danish");
+    return 0; //this is return
+}
