a5c986ad9365caeda48868ad33c0a55263b8ce12
DAN786000 <danishjmohammad@gmail.com>
2026-06-19 07:33:15 -0700

    Added topic 2

---
+++ Chapter 2/02_arithmatic_operators.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a=6;
+    int b=3;
+    int c=a+b;
+    printf("The value of a is %d and the value of b is %d and sum is %d\n",a,b,c);
+    //The modulus operator is used to get the remainder
+    printf("The remainder when a is divided with b is %d\n",a%b);
+
+    //This does not work exponentitation in c
+    //int d=a^b
+      
+    
+    
+    return 0;
+}
