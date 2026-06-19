733f48190708fc62d60490262b30d8bbf0c00088
DAN786000 <danishjmohammad@gmail.com>
2026-06-19 06:45:27 -0700

    Added problem2

---
+++ Chapter 1 Practice set/02_problem2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int r=6;
+    int height=6;
+    printf("The area of the circle is %d is %f\n",r,3.14*r*r);
+    printf("The height of the cylinder is %d is %f",height,3.14*r*r*height);
+    return 0;
+}
+    
