7994426bca370fec44bc2b3ac780305c1ce77e87
DAN786000 <danishjmohammad@gmail.com>
2026-06-19 08:38:16 -0700

    Added topic 1

---
+++ Chapter 4/01_loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    //problem 1:print happy birthday 1 Lakh times
+    int i=0;
+    while(i<100000){
+        printf("Happy birthday\n");
+        i= i+1;
+    }
+    return 0;
+}
