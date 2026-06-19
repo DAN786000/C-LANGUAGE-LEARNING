f173aed35ce42bd01086034c3d044e660e78a199
DAN786000 <danishjmohammad@gmail.com>
2026-06-19 08:18:10 -0700

    Added problem 1

---
+++ Chapter 2 Practice set/01_problem.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+//Which of the following is invalid in C? 
+int a=1; int b = a; // It is correct.
+int v = 3*3; // It is also correct.
+char dt = ‘21 dec 2020’; //Wrong! (These is not more than 1 character)
+    return 0;
+}
