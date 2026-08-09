a3cb8b28a107385c78bbedae92c4cb0cd1aa338a
DAN786000 <danishjmohammad@gmail.com>
2026-08-08 17:32:27 -0700

    Added chapter 7 practice set

---
+++ Chapter 7 Practice set/02_problem2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+//  If S[3] is a 1-D array of integers then *(S+3) refers to the third element: 
+// (i) True. 
+// (ii) False. //The correct answer is false.(Because *(S+3) refers to the fourth element).
+// (iii) Depends.
+    return 0;
+}
