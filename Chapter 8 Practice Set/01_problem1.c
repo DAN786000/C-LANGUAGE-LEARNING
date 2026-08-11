414a7864c334acf820b2f36d68f3f58a982ae7c4
DAN786000 <danishjmohammad@gmail.com>
2026-08-10 20:08:53 -0700

    Added chapter 8 practice set

---
+++ Chapter 8 Practice Set/01_problem1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+// 1. Which of the following is used to appropriately read a multi-word string. 
+// 1. gets() // This is the correct answer because we use gets() to read a multi-word string.
+// 2. puts() 
+// 3. printf() 
+// 4. scanf()
+    return 0;
+}
